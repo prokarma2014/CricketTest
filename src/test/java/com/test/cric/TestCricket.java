938d929f09467997f13b54cd7b5a0c9890d45b15
prokarma2014 <deepeshpk@gmail.com>
2015-03-24 12:05:15 +0530

    Update TestCricket.java
    Increase in time period

--- src/test/java/com/test/cric/TestCricket.java
+++ src/test/java/com/test/cric/TestCricket.java
@@ -28,19 +28,19 @@ public class TestCricket {
 	@Test
 	public void cric1Signup() throws InterruptedException{
 		driver.findElement(By.linkText("Sign Up")).click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("firstName")).sendKeys("pro");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("lastName")).sendKeys("karma");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("employeeId")).sendKeys("15528");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("emailId")).sendKeys("prk@prokarma.com");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("userName")).sendKeys("pkarma");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("password")).sendKeys("pkarma");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("repeatPassword")).sendKeys("pkarma");
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("/html/body/div/form/table/tbody/tr[8]/td/input")).click();
@@ -59,10 +59,11 @@ public class TestCricket {
 		
 		
 		driver.findElement(By.name("userName")).sendKeys("pkarma");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("password")).sendKeys("pkarma");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.tagName("input")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("/html/body/form/div[2]/table/tbody/tr[3]/td/input")).click();
 		Thread.sleep(2000);
 		
@@ -74,14 +75,15 @@ public class TestCricket {
 		
 		cric2Login();
 		driver.findElement(By.linkText("Team Selection")).click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("teamName")).sendKeys("Pinch Hitters");
+		Thread.sleep(2000);
 		driver.findElement(By.name("teamID")).sendKeys("10002");
 		Thread.sleep(2000);
 		jse.executeScript("scroll(0, 250)");
 		driver.findElement(By.name("captainName")).sendKeys("John");
 		driver.findElement(By.name("viceCaptainName")).sendKeys("Deepesh");
-		Thread.sleep(1000);
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("/html/body/form/div/table/tbody/tr[6]/td/input")).click();
 		
 		Thread.sleep(3000);
@@ -93,7 +95,7 @@ public class TestCricket {
 		
 		cric2Login();
 		driver.findElement(By.linkText("Enrollment Of Player")).click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("name")).sendKeys("deepeshk");
 		Thread.sleep(1000);
 		driver.findElement(By.name("employeeId")).sendKeys("1575");
@@ -108,7 +110,7 @@ public class TestCricket {
 		Thread.sleep(1000);
 		driver.findElement(By.name("petName")).sendKeys("deepu");
 		
-		Thread.sleep(2000);
+		Thread.sleep(3000);
 		
 		driver.findElement(By.id("enroll")).click();
 		Thread.sleep(3000);
@@ -122,10 +124,11 @@ public class TestCricket {
 		
 		
 		driver.findElement(By.name("userName")).sendKeys("jmuh");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.name("password")).sendKeys("pkarma");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		driver.findElement(By.tagName("input")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("/html/body/form/div[2]/table/tbody/tr[3]/td/input")).click();
 		Thread.sleep(2000);
 		
